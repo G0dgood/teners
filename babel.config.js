678c3884d7278cb9e8dc834438c10916b039b747
user <chinedu.go@gmail.com>
2024-05-20 19:23:46 +0100

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      "babel-preset-expo",
-      "module:metro-react-native-babel-preset",
-      "@babel/preset-typescript",
-    ],
+    presets: ["babel-preset-expo"],
     plugins: [
-      "@babel/plugin-proposal-class-properties",
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
       "@babel/plugin-proposal-object-rest-spread",
     ],
   };
